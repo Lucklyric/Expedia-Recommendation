c3e6f8dd0490b3f960cd479c805354b34babe1bc
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2017-05-21 00:23:35 -0600

    Update v7 version

--- scripts/dl_v7.py
+++ scripts/dl_v7.py
@@ -11,8 +11,8 @@ import tensorflow.contrib.keras as keras
 TRAINING = 0
 TESTING = 1
 INFERENCE = 2
-VERSION = "v6"
-MODE = 2
+VERSION = "v7"
+MODE = 0
 NUM_EPOCHS = 1000000
 # MODE = TESTING
 # NUM_EPOCHS = 1
@@ -60,7 +60,7 @@ class RDWModel(object):
         with tf.name_scope("Input" + self.pos_fix):
             self.learning_rate = tf.placeholder(tf.float64, name="LR")
             if MODE == TRAINING:
-                feature, label = read_and_decode(["../data/train-13.tfrecords"], num_epochs=NUM_EPOCHS)
+                feature, label = read_and_decode(["../data/train-13-all-book-type.tfrecords"], num_epochs=NUM_EPOCHS)
                 self.feature, self.label_batch = tf.train.shuffle_batch([feature, label], batch_size=128, num_threads=3,
                                                                         capacity=2000,
                                                                         min_after_dequeue=1000,
@@ -78,27 +78,35 @@ class RDWModel(object):
 
         with tf.name_scope("Des_Embedding"):
 
+            # booking type
+            booking_type = self.feature[:, 20:21]
+
             # user location city
-            u_loc_city = self.add_bucket_embedding(tf.cast(self.feature[:, 9], tf.int64), 100000, 8, "u_loc_city")
-            u_loc_city = self.add_norm(u_loc_city)
-            u_loc_city = self.add_fc_stack_layers(u_loc_city, [50, 50])
+            # u_loc_city = self.add_bucket_embedding(tf.cast(self.feature[:, 9], tf.int64), 100000, 8, "u_loc_city")
+            # u_loc_city = self.add_norm(u_loc_city)
+            # u_loc_city = self.add_fc_stack_layers(u_loc_city, [50, 50])
+            u_loc_city = self.add_norm(self.feature[:, 9:10])
 
             # orig destination distance
             orig_destination_distance = self.feature[:, 10:11]
             orig_destination_distance = self.add_norm(orig_destination_distance)
 
             # orig destination
-            des_embedding_feature = tf.nn.embedding_lookup(self.destination_embedding,
-                                                           tf.cast(self.feature[:, 18], tf.int64))
-            des_embedding_feature = self.add_norm(des_embedding_feature)
-            des_embedding_feature = self.add_fc_stack_layers(des_embedding_feature, [128, 256])
+            # des_embedding_feature = tf.nn.embedding_lookup(self.destination_embedding,
+            #                                                tf.cast(self.feature[:, 18], tf.int64))
+            # des_embedding_feature = self.add_norm(des_embedding_feature)
+            # des_embedding_feature = self.add_fc_stack_layers(des_embedding_feature, [128, 256])
+            des_embedding_feature = self.add_norm(self.feature[:, 18:19])
 
-            h_contry = self.add_bucket_embedding(tf.cast(self.feature[:, 23], tf.int64), 1000, 8, "h_contry")
-            h_contry = self.add_norm(h_contry)
-            h_contry = self.add_fc_stack_layers(h_contry, [50, 50])
-            h_market = self.add_bucket_embedding(tf.cast(self.feature[:, 24], tf.int64), 100000, 8, "h_market")
-            h_market = self.add_norm(h_market)
-            h_market = self.add_fc_stack_layers(h_market, [50, 50])
+            # h_contry = self.add_bucket_embedding(tf.cast(self.feature[:, 23], tf.int64), 1000, 8, "h_contry")
+            # h_contry = self.add_norm(h_contry)
+            # h_contry = self.add_fc_stack_layers(h_contry, [50, 50])
+            h_contry = self.add_norm(self.feature[:, 23:24])
+
+            # h_market = self.add_bucket_embedding(tf.cast(self.feature[:, 24], tf.int64), 100000, 8, "h_market")
+            # h_market = self.add_norm(h_market)
+            # h_market = self.add_fc_stack_layers(h_market, [50, 50])
+            h_market = self.add_norm(self.feature[:, 24:25])
 
 
             # self.stack_features = tf.concat([
@@ -117,18 +125,22 @@ class RDWModel(object):
         with tf.name_scope("FC"):
             # branch od ulc
             self.branch_od_ulc = self.add_fc_stack_layers(tf.concat([orig_destination_distance,
-                                                                     u_loc_city], axis=1), [128, 256])
+                                                                     u_loc_city, booking_type], axis=1), [128, 256]
+                                                          )
 
             # branch dest features
             self.branch_search_dest = self.add_fc_stack_layers(tf.concat([des_embedding_feature,
                                                                           h_contry,
-                                                                          h_market], axis=1), [512, 512])
+                                                                          h_market, booking_type], axis=1), [512, 512]
+                                                               )
 
             # search des id only
-            self.branch_search_dest_only = self.add_fc_stack_layers(des_embedding_feature, [128, 512])
+            self.branch_search_dest_only = self.add_fc_stack_layers(
+                tf.concat([des_embedding_feature, booking_type], axis=1), [128, 512])
 
             # hotel country
-            self.branch_hotel_country = self.add_fc_stack_layers(h_contry, [128, 128])
+            self.branch_hotel_country = self.add_fc_stack_layers(tf.concat([h_contry, booking_type], axis=1),
+                                                                 [128, 128])
 
         with tf.name_scope("Output"):
             self.b1_weight = tf.Variable(tf.ones([], dtype=tf.float64))
@@ -170,14 +182,12 @@ class RDWModel(object):
             self.b4_loss = tf.reduce_mean(
                 tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.branch_hotel_country,
                                                                labels=self.label_batch))
-            self.combine_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.output,
-                                                                                              labels=self.label_batch))
+            self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.output,
+                                                                                      labels=self.label_batch))
             # self.loss = (tf.multiply(self.b1_loss, self.b1_weight) + \
             #              tf.multiply(self.b2_loss, self.b2_weight) + \
             #              tf.multiply(self.b3_loss, self.b3_weight) + \
             #              tf.multiply(self.b4_loss, self.b4_weight)) * 0.5 + 0.5 * self.combine_loss
-
-            self.loss = self.combine_loss + 0.5 * (self.b1_loss + self.b2_loss + self.b3_loss + self.b4_loss)
 
             tf.summary.scalar('b1_loss', self.b1_loss)
             tf.summary.scalar('b1_weight', self.b1_weight)
@@ -187,11 +197,14 @@ class RDWModel(object):
             tf.summary.scalar('b3_weight', self.b3_weight)
             tf.summary.scalar('b4_loss', self.b4_loss)
             tf.summary.scalar('b4_weight', self.b4_weight)
-            tf.summary.scalar('combine_loss', self.combine_loss)
             tf.summary.scalar('loss', self.loss)
 
         with tf.name_scope("Train"):
             self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+            self.b1_train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.b1_loss)
+            self.b2_train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.b2_loss)
+            self.b3_train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.b3_loss)
+            self.b4_train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.b4_loss)
             self.increase_step = self.global_step.assign_add(1)
 
     def _add_fc_layer(self, layer_input, size, activation_fn=tf.nn.relu, dropout=True, norm=True):
@@ -245,22 +258,22 @@ class RDWModel(object):
         step = 0
         try:
             while not coord.should_stop():
-                _, _, merged_summary, step_value, loss_value, b1_loss_value, b2_loss_value, \
-                b3_loss_value, b4_loss_value, combine_loss_value, net_output = sess.run(
-                    [self.train_op, self.increase_step, merged, self.global_step, self.loss, self.b1_loss,
-                     self.b2_loss, self.b3_loss, self.b4_loss, self.combine_loss,
-                     self.output], feed_dict={
+                _, _, _, _, _, _, merged_summary, step_value, loss_value, b1_loss_value, b2_loss_value, \
+                b3_loss_value, b4_loss_value = sess.run(
+                    [self.train_op, self.b1_train_op, self.b2_train_op, self.b3_train_op, self.b4_train_op,
+                     self.increase_step, merged, self.global_step, self.loss, self.b1_loss,
+                     self.b2_loss, self.b3_loss, self.b4_loss
+                     ], feed_dict={
                         self.learning_rate: LEARNING_RATE
                     })
                 writer.add_summary(merged_summary, global_step=step_value)
+                step = step_value
 
-                if step % 100 == 0:
+                if step % 10 == 0:
                     saver.save(sess, "model/" + VERSION + "/model.ckpt")
                     print (
-                    "Step %d: loss= %.4f, b1_loss=%.4f, b2_loss=%.4f, b3_loss=%.4f, b4_loss=%.4f, combine_loss=%.4f" % (
-                        step, loss_value, b1_loss_value, b2_loss_value, b3_loss_value, b4_loss_value,
-                        combine_loss_value))
-                step += len(net_output)
+                        "Step %d: loss= %.4f, b1_loss=%.4f, b2_loss=%.4f, b3_loss=%.4f, b4_loss=%.4f" % (
+                            step, loss_value, b1_loss_value, b2_loss_value, b3_loss_value, b4_loss_value))
         except tf.errors.OutOfRangeError:
             print ("Done training for %d epochs, %d steps." % (NUM_EPOCHS, step))
         finally:
@@ -275,7 +288,6 @@ class RDWModel(object):
                 mAP, _, net_output, feature_value, target_label, num_correct = sess.run(
                     [self.mAP, self.mAP_update, self.output, self.feature, self.label_batch,
                      self.num_correct_prediction])
-                test_out = np.argmax(net_output, axis=1)
                 correnct_entry += num_correct
                 # print test_out
                 # print feature_value
